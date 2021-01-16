e1df6594dd881e48ad82383ea7ddc3fd6fca112b
Alex <sashafadeev@gmail.com>
2021-01-16 15:52:00 +0200

    feat: implement lession 1 tasks

---
+++ SayName/SayName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SayName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            DateTime date = DateTime.Today;
+
+            Console.WriteLine($"Hello {userInput}, today is {date}");
+            Console.WriteLine("Press any key to close this window . . .");
+
+            // This need to keep console window open until user will press any key afer programm display message
+            Console.ReadLine();
+        }
+    }
+}

4058be724a60033f99b0dca83fe1e35d0afedb5c
Alex <sashafadeev@gmail.com>
2021-01-17 00:13:24 +0200

    fix: add date format

--- SayName/SayName/Program.cs
+++ SayName/SayName/Program.cs
@@ -8,8 +8,9 @@ namespace SayName
         {
             string userInput = Console.ReadLine();
             DateTime date = DateTime.Today;
+            string dateString = date.ToString("dd MMMM yyyy");
 
-            Console.WriteLine($"Hello {userInput}, today is {date}");
+            Console.WriteLine($"Hello {userInput}, today is {dateString}");
             Console.WriteLine("Press any key to close this window . . .");
 
             // This need to keep console window open until user will press any key afer programm display message

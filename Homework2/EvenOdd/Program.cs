b7eb8b175ffd1a68fe9b01478c365a362308c8d0
Alex <sashafadeev@gmail.com>
2021-01-23 21:55:52 +0200

    feat: implement EvenOdd project

---
+++ Homework2/EvenOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any integer number");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out var number))
+            {
+                string resultString = number % 2 == 0 ? "Even" : "Odd";
+                Console.WriteLine($"Number {number} is {resultString}");
+                return;
+            }
+            
+            Console.WriteLine("Something went wrong. Perhaps you've entered wrong character");
+        }
+    }
+}

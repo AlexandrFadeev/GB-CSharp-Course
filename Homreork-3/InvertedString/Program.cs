797bec2ddac9c9dadf5b59da5eef6210b1409548
Alex <sashafadeev@gmail.com>
2021-01-24 20:47:03 +0200

    feat: implement inverted string project

---
+++ Homreork-3/InvertedString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace InvertedString
+{
+    class Program
+    {
+        private const string Text = "Hello";
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Inverted text Hello: [ {InvertString(Text)} ]");
+        }
+
+        static string InvertString(string text)
+        {
+            var resultStringBuilder = new StringBuilder();
+            
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                resultStringBuilder.Append(text[i]);
+            }
+            return resultStringBuilder.ToString();
+        }
+    }
+}

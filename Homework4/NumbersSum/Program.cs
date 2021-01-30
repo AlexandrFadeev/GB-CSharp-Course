c32341f38794504e290c5942ae5a8defbd46de79
Alex <sashafadeev@gmail.com>
2021-01-31 00:05:19 +0200

    feat: implement homework4 project2, sum of entered numbers

---
+++ Homework4/NumbersSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace NumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Console.ReadLine();
+
+            if (userInput == null)
+            {
+                return;
+            }
+
+            var components = userInput.Split(' ');
+            PrintSum(components);
+        }
+
+        private static void PrintSum(string[] components)
+        {
+            var sum = 0;
+
+            foreach (var component in components)
+            {
+                if (int.TryParse(component, out var i))
+                {
+                    sum += i;
+                }
+            }
+            
+            Console.WriteLine($"Sum of elements: {sum}");
+        }
+    }
+}

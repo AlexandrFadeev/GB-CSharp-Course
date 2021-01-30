b4c355f632f281ef8e15686a7ce94430c7328b1d
Alex <sashafadeev@gmail.com>
2021-01-31 00:52:58 +0200

    wip: add starter project of homework4 projecr4, recursive fibonacci numbers

---
+++ Homework4/Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFibonacciSequence();
+        }
+
+        private static void PrintFibonacciSequence()
+        {
+            
+        }
+    }
+}

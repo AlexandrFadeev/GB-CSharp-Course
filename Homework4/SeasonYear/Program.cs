911b317bd061a3fe9ffaabd42d16d6603b53d67e
Alex <sashafadeev@gmail.com>
2021-01-31 00:10:28 +0200

    wip: add starter project for homework4 project3, print season of year

---
+++ Homework4/SeasonYear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeasonYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

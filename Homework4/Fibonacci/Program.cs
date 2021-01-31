53887610a4b76cf85a730d07791c5e2a0847f3d6
Alex <sashafadeev@gmail.com>
2021-01-31 12:21:10 +0200

    feat: implement homework4 lession4, fibonacci sequence

--- Homework4/Fibonacci/Program.cs
+++ Homework4/Fibonacci/Program.cs
@@ -6,12 +6,33 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            PrintFibonacciSequence();
+            var program = new Program();
+            program.PrintFibonacciSequence(0, 1, 100, true);
         }
 
-        private static void PrintFibonacciSequence()
+        private void PrintFibonacciSequence(long previousResult, long result, int numberOfSteps, bool isInitial)
         {
+            if (numberOfSteps <= 0)
+            {
+                return;
+            }
             
+            var sum = previousResult + result;
+            // If sum has negative value, that means value is overflowed
+            if (sum < 0)
+            {
+                Console.WriteLine("Stop execution. Fibonacci number is overflowed");
+                return;
+            }
+
+            if (isInitial)
+            {
+                Console.WriteLine($"{previousResult}");
+                Console.WriteLine($"{result}");
+            }
+
+            Console.WriteLine($"{previousResult + result}");
+            PrintFibonacciSequence(result, sum, numberOfSteps - 1, false);
         }
     }
 }

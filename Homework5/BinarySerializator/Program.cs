0ad6a630c1b6edb8560df7ed29ae59a080584ebc
Alex <sashafadeev@gmail.com>
2021-02-01 00:02:41 +0200

    feat: implement homework5 project3, binary searization

--- Homework5/BinarySerializator/Program.cs
+++ Homework5/BinarySerializator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace BinarySerializator
 {
@@ -6,7 +8,12 @@ namespace BinarySerializator
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter sequence of numbers");
+            var userInput = Console.ReadLine();
+
+            var formatter = new BinaryFormatter();
+            var fileStream = new FileStream("numbers.bin", FileMode.OpenOrCreate);
+            formatter.Serialize(fileStream, userInput ?? throw new InvalidOperationException());
         }
     }
 }

31e7fd2d9047df5671d921e591ec7defa698ec20
Alexandr Fadeev <alex@myluckyday.fr>
2021-02-24 18:44:53 +0200

    feat: implement homework2, task2 binary search

---
+++ Algorithms2/Algorithms2/BinarySearch.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms2
+{
+    public class BinarySearch
+    {
+        private enum Compare
+        {
+            Less = -1,
+            Equal,
+            Greater
+        }
+        
+        public int? SearchElementFromCollection<T>(List<T> array, T searchItem) where T : IComparable
+        {
+            if (array.Count == 0)
+            {
+                return null;
+            }
+            
+            var leftAnchor = 0;
+            var rightAnchor = array.Count - 1;
+
+            while (leftAnchor <= rightAnchor)
+            {
+                var midIndex = Convert.ToInt32((leftAnchor + rightAnchor) * 0.5);
+                if (array[midIndex].Equals(searchItem))
+                {
+                    return midIndex;
+                }
+                
+                if (searchItem.CompareTo(array[midIndex]) == (int)Compare.Less)
+                {
+                    rightAnchor = midIndex - 1;
+                }
+                else
+                {
+                    leftAnchor = midIndex + 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}

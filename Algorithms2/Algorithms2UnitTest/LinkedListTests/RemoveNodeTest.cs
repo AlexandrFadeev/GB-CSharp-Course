3a5ba0aaa7cdca66f92800678e770372e174cba8
Alex <sashafadeev@gmail.com>
2021-03-04 11:17:24 +0200

    wip: work on RemoveNode unit test

--- Algorithms2/Algorithms2UnitTest/LinkedListTests/RemoveNodeTest.cs
+++ Algorithms2/Algorithms2UnitTest/LinkedListTests/RemoveNodeTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using Algorithms2.LinkedList;
 
@@ -78,14 +80,80 @@ namespace Algorithms2UnitTest.LinkedListTests
             
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [Test]
+        public void RemoveTail_FailingTest()
+        {
+            var lastIndex = _nodes.Length - 1;
+            var tail = _nodes[lastIndex];
+
+            var actualResult = _linkedList.RemoveLastNode();
+            
+            Assert.AreNotEqual(tail, actualResult);
+        }
+
+        [Test]
+        public void RemoveTail_FromSingleElementList_PassingTest()
+        {
+            var node = new Node(0);
+            var linkedList = new DoublyLinkedList();
+            linkedList.AddNode(node);
+
+            var actualResult = linkedList.RemoveLastNode();
+            
+            Assert.IsNull(actualResult);
+        }
+
+        [Test]
+        public void RemoveTail_FromSingleElementList_FailingTest()
+        {
+            var node = new Node(0);
+            var linkedList = new DoublyLinkedList();
+            linkedList.AddNode(node);
+
+            var actualResult = linkedList.RemoveLastNode();
+            
+            Assert.AreNotEqual(node, actualResult);
+        }
+
+        [TestCase(2)]
+        public void RemoveNode_PassingTest(int removeIndex)
+        {
+            var nodeToRemove = _nodes[removeIndex];
+            _linkedList.RemoveNode(nodeToRemove);
+
+            var expectedLinkedList = DoublyLinkedListWithNodesAndRemovedIndex(_nodes, removeIndex);
+            
+            Assert.AreEqual(expectedLinkedList, _linkedList);
+        }
         
-        private DoublyLinkedList DoublyListWithNodes(Node[] nodes)
+        private DoublyLinkedList DoublyListWithNodes(IEnumerable<Node> nodes)
         {
             var linkedList = new DoublyLinkedList();
 
             foreach (var node in nodes)
             {
                 linkedList.AddNode(node);
+            }
+
+            return linkedList;
+        }
+
+        private DoublyLinkedList DoublyLinkedListWithNodesAndRemovedIndex(IEnumerable<Node> nodes, int removeIndex)
+        {
+            var linkedList = new DoublyLinkedList();
+            var index = 0;
+
+            while (index < nodes.Count() - 1)
+            {
+                if (index == removeIndex)
+                {
+                    index++;
+                    continue;
+                }
+                
+                linkedList.AddNode(index);
+                index++;
             }
 
             return linkedList;

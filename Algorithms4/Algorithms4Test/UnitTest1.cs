63c27a1e2b794d2d5bb6e19914af123c268e0de3
Alexandr Fadeev <alex@myluckyday.fr>
2021-03-07 11:29:08 +0200

    chor: add Algorithms4 initial unit test project

---
+++ Algorithms4/Algorithms4Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Algorithms4Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
